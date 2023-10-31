8b22f74dd7aaedd8f57d703231764828e3d0cfb0
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-10-31 17:47:23 +0530

    Adding a Form

---
+++ src/components/Cart/cart.js
@@ -0,0 +1,13 @@
+import classes from './Cart.module.css'
+const Cart = props => {
+   const cartItems=[{id:'c1', name:'Sushi', amount: 2, price:12.99},
+].map((item) => <li>{item.name}</li>);
+    return(
+
+        <div>
+            cartitems
+        </div>
+    );
+};
+
+export default Cart;
