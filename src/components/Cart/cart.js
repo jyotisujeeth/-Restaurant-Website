2f7565fc7d68527d7a26f0bee51ba6b7b9acfef8
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-11-03 04:25:17 +0530

    Cart function edited

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,22 +1,23 @@
-import Modal from '.../UI/Modal';
-import classes from './Cart.module.css'
-const Cart = props => {
-   const cartItems= <ul className={classes['cart-item']}>[{id: 'c1', name:'Sushi', amount: 2, price:12.99},
-].map((item) => <li>{item.name}</li>)</ul>;
-    return(
+import classes from "./CartItem.module.css";
 
-        <Modal>
-           {cartitems}
-            <div>
-                <span>Total Amount</span>
-                <span>35.62</span>
-            </div>
-            <div className={classes.action}>
-                <button className={classes['button--alt']}>close</button>
-                <button className={classes.button}>order</button>
-            </div>
-        </Modal>
-    );
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={classes["cart-item"]}>
+      <div>
+        <h2>{props.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{price}</span>
+          <span className={classes.amount}>x {props.amount}</span>
+        </div>
+      </div>
+      <div className={classes.actions}>
+        <button onClick={props.onRemove}>−</button>
+        <button onClick={props.onAdd}>+</button>
+      </div>
+    </li>
+  );
 };
 
-export default Cart;
+export default CartItem;
