1139da5cf8f247e87d10de6e7581e64c9f62d1ac
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2023-11-06 05:07:54 +0530

    store msgasge

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,13 +2,13 @@ import { useContext } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css'
-import CartContext from '../store/cart-context';
+//mport CartContext from '../store/cart-context';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) =>{
   const CartContext = useContext(CartContext);
 
-  const numberOfCartItem = CratCtx.item.reduce((curNumber, item) => {
+  const numberOfCartItem = CartContext.item.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
     return (
@@ -18,7 +18,7 @@ const HeaderCartButton = (props) =>{
         </span>
         <span>Your Cart</span>
         <span className={classes.badge}>{numberOfCartItem}</span>
-        <span>{CartContextt.message}</span>
+        <span>{CartContext.message}</span>
       </button>
     );
 };
